a8bf6fa8e6757d4a659ae0bc940b0c6a48ab72ee
sc20xj <sc20xj@leeds.ac.uk>
2022-05-13 23:01:23 +0800

    improved sdl

--- main.c
+++ main.c
@@ -6,80 +6,86 @@
 #include <ctype.h>
 #include <SDL2/SDL.h>
 
+#include <stdbool.h>
 #define terminate 0
 #define still 1
-#define delay 100
+#define delay 1000
+
 
 int Width;
 int High;
-SDL_Renderer* renderer = NULL;
-SDL_Window *window = NULL;
+int key=1;
 
-SDL_Texture* Texture = NULL;
-SDL_Texture* Texture2 = NULL;
-SDL_Surface* surface=NULL;
 
-void init(){
- if (SDL_Init(SDL_INIT_VIDEO) != 0) 
-        fprintf(stderr, "SDL_Init: %s\n", SDL_GetError());
-    
-    atexit(SDL_Quit); // set for clean-up on exit
-    SDL_CreateWindowAndRenderer(Width*100, High*100, 0, &window, &renderer);
-    SDL_SetWindowTitle( window, "Circles eat squares");
-    surface = SDL_CreateRGBSurface(0, Width*100, High*100, 32, 255, 255, 255, 255);
+void input(void)
+{
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderClear(renderer);
+    SDL_PumpEvents();
+
+    if (state[SDL_SCANCODE_UP]) {
+        
+    } 
+    else if (state[SDL_SCANCODE_DOWN]) {
+        key=0;
+    } 
+    else if (state[SDL_SCANCODE_LEFT]) {
+        key=2;
+    } 
+    else if (state[SDL_SCANCODE_RIGHT]) {
+        key=3;
+    } 
+    else if (state[SDL_SCANCODE_ESCAPE]) {
+        exit(0);
+    }
+    return;
 }
 
 
-
-
-
-void show(int **cells)
-{
+void show(SDL_Renderer* sdlRenderer,SDL_Window* sdlWindow,SDL_Texture* sdlTexture,int** cells){
+       
     SDL_Rect rect;
     rect.x=0;
     rect.y=0;
     rect.h=100;
     rect.w=100;
+    SDL_SetRenderTarget(sdlRenderer,sdlTexture); // 改变渲染目标为纹理
+    SDL_SetRenderDrawColor(sdlRenderer,255,255,255,255); // 设置纹理颜色(颜色为RGBA)
+    SDL_RenderClear(sdlRenderer); // 清空渲染器
     for(int i=0;i<High;i++){
         for(int j=0;j<Width;j++){
             if(cells[i][j]==0){
-           
-            SDL_FillRect(surface,&rect, SDL_MapRGB(surface->format, 255, 0, 0));
-            printf("fuck");
-            Texture = SDL_CreateTextureFromSurface(renderer,surface);
-             printf("fuck");
-             SDL_RenderCopy(renderer, Texture, NULL, &rect);
-            //    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-            //    SDL_RenderFillRect(renderer, &rect);
-            //    SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
-            //    SDL_RenderDrawRect(renderer, &rect);
+              
+          
+               SDL_SetRenderDrawColor(sdlRenderer, 255, 255, 255, 255);
+               SDL_RenderFillRect(sdlRenderer, &rect);
+               SDL_SetRenderDrawColor(sdlRenderer, 100, 100, 100, 255);
+               SDL_RenderDrawRect(sdlRenderer, &rect);
            
                rect.x+=100;
                
             }
             if(cells[i][j]==1){
-                // SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-                // SDL_RenderFillRect(renderer, &rect);
-                //  SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
-                //  SDL_RenderDrawRect(renderer, &rect);
-            SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, 255, 255, 255));
-            Texture2 = SDL_CreateTextureFromSurface(renderer,surface);
-             SDL_RenderCopy(renderer, Texture2, NULL, &rect);
-                rect.x+=100;
-
-                
+              
+                SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 255);
+                SDL_RenderFillRect(sdlRenderer, &rect);
+                 SDL_SetRenderDrawColor(sdlRenderer, 100, 100, 100, 255);
+                 SDL_RenderDrawRect(sdlRenderer, &rect);
+       
+                 rect.x+=100;
             }
         }
         rect.x=0;
         rect.y+=100;
     }
-    SDL_RenderPresent(renderer);
-    SDL_Delay(delay);
-    return;
+     SDL_SetRenderTarget(sdlRenderer, NULL);		// 恢复默认渲染目标,将要渲染的目标设置为默认窗口
+	SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, NULL)	;	// 将纹理数据复制给渲染器
+    SDL_RenderPresent(sdlRenderer);
+    SDL_Delay(1500);
+
 }
+
+
 
 
 int ** copy(int **cells){
@@ -184,7 +190,12 @@ int optionChoice( void ) {
 }
      }
 
-  
+void close(SDL_Window* sdlWindow,SDL_Texture*sdlTexture,SDL_Renderer * sdlRenderer){
+    SDL_DestroyTexture(sdlTexture);			// 释放纹理
+	SDL_DestroyRenderer(sdlRenderer);		// 释放渲染器
+	SDL_DestroyWindow(sdlWindow);			// 释放窗口
+
+}
     
 
 
@@ -311,10 +322,11 @@ int **create(){
 
 int ** load(FILE* file){
 
-    char buff[100];
+    char buff[1000];
     char c;
     int j=0;
     int y;
+    int m=100;
     int **temp;
      if (file == NULL ){
     printf("Error\nBook file does not exist");
@@ -337,34 +349,61 @@ int ** load(FILE* file){
 	for(int i = 0; i < High; i++){
 		cells[i] = (int *)malloc(Width * sizeof(int));
 	}
-        
-	for(int i=0;i<High;i++){
-        for(int j=0;j<Width;j++){
-            c=fgetc(file);
+    for(int i=0;i<High;i++){
+         fgets(buff,1000,file);
+         for(int j=0;j<Width;j++){
+            c=buff[j];
             cells[i][j]=c-'0';
         }
     }
+        
     temp=copy(cells);
     updateWithoutInput(cells);
     y=judge(cells,temp);
-    free(cells);
+    freecell(cells);
     cells=copy(temp);
-    free(temp);
+    freecell(temp);
     
     printf("Previous world loaded successfully\n");
     
     while(j==0){
     printf("previous world:\n");
-    init();
-    show(cells);
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+	// 创建窗口
+	SDL_Window *sdlWindow = SDL_CreateWindow("drawRandRect",  SDL_WINDOWPOS_CENTERED,  SDL_WINDOWPOS_CENTERED, Width*100, High*100, SDL_WINDOW_SHOWN);
+
+
+	// 创建渲染器
+	SDL_Renderer *sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, 0);
+
+
+	// 创建纹理
+	SDL_Texture *sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, Width*100, High*100);
+	SDL_Event sdlEvent;
+
+    printf("The origin world:\n");
+    while(m==100){
+   while (SDL_PollEvent(&sdlEvent)){
+            switch (sdlEvent.type) {
+                case SDL_QUIT:{
+                    break;
+                }    
+        
+            }
+        }
+
+    show(sdlRenderer,sdlWindow,sdlTexture, cells);
+   /////
     if(y==0){
         printf("This world is terminated\n");
     }
     printf("Do you want to start with this?(0 for no and 1 for yes)\n");
     printf("Choice:");
-    int m;
-    m=optionChoice();
     
+    m=optionChoice();
+    }
+   close(sdlWindow,sdlTexture,sdlRenderer);
     if(m==-1){
              printf("\nYou should input 0 or 1\n\n");
                 }
@@ -557,22 +596,210 @@ void store(FILE* file,int **cells){
 
 
 
-void startup(int **cells)  
+void change(int x,int y,int**cells){
+    if(cells[x][y]==0){
+        cells[x][y]=1;
+    }
+    else{
+        cells[x][y]=0;
+    }
+}
+
+int run2(int**cells){
+     int q=1;
+    int d=-100;
+    int **last=NULL;
+    
+    
+    if (SDL_Init(SDL_INIT_EVERYTHING) == -1) { return -1; }
+
+	// 创建窗口
+	SDL_Window *sdlWindow = SDL_CreateWindow("drawRandRect",  SDL_WINDOWPOS_CENTERED,  SDL_WINDOWPOS_CENTERED, Width*100, High*100, SDL_WINDOW_SHOWN);
+	if (!sdlWindow) { return -1; }
+
+	// 创建渲染器
+	SDL_Renderer *sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, 0);
+	if (!sdlRenderer) { return -1; }
+
+	// 创建纹理
+	SDL_Texture *sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, Width*100, High*100);
+	if (!sdlTexture) { return -1; }
+
+    printf("The origin world:\n");
+    show(sdlRenderer,sdlWindow,sdlTexture, cells);
+    
+    SDL_Event event;
+         while(1){
+             while (SDL_PollEvent(&event)){
+            switch (event.type) {
+                case SDL_QUIT:{
+        
+                    break;
+                }    
+               case SDL_MOUSEBUTTONDOWN:{
+                    int mouse_x = event.button.x/Width;
+                    int mouse_y = event.button.y/High;
+                    if(event.button.button == SDL_BUTTON_LEFT ){
+                        change(mouse_x,mouse_y,cells);
+                    }
+                }
+                case SDL_MOUSEMOTION:{
+                     int mouse_x = event.button.x/Width;
+                    int mouse_y = event.button.y/High;
+                    if(event.button.button == SDL_BUTTON_LEFT ){
+                        change(mouse_x,mouse_y,cells);
+                    }
+                }
+               
+            }
+        }
+
+        if(last!=NULL){
+        d=judge(cells,last);
+        }
+        if(d==0){
+            printf("It is terminate\n");
+               close(sdlWindow,sdlTexture,sdlRenderer);
+            return terminate;
+        }
+        printf("step %d:\n",q);
+        free(last);
+        last=copy(cells);
+      
+         show(sdlRenderer,sdlWindow,sdlTexture, cells);
+        updateWithoutInput(cells);
+        
+         printf("\n");
+         q+=1;
+       
+    }
+
+    
+
+
+
+}
+
+
+
+int run1(int **cells,int y)
+
 {
+    
+    int d=-100;
+    int **last=NULL;
+    int m;
+    int **my=NULL;
+    if (SDL_Init(SDL_INIT_EVERYTHING) == -1) { return -1; }
+
+	// 创建窗口
+	SDL_Window *sdlWindow = SDL_CreateWindow("drawRandRect",  SDL_WINDOWPOS_CENTERED,  SDL_WINDOWPOS_CENTERED, Width*100, High*100, SDL_WINDOW_SHOWN);
+	if (!sdlWindow) { return -1; }
+
+	// 创建渲染器
+	SDL_Renderer *sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, 0);
+	if (!sdlRenderer) { return -1; }
+
+	// 创建纹理
+	SDL_Texture *sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, Width*100, High*100);
+	if (!sdlTexture) { return -1; }
+
+    printf("The origin world:\n");
+    show(sdlRenderer,sdlWindow,sdlTexture, cells);
+    
+    SDL_Event event;
+
+    //certain step	
+        int z=0;
+        while(z<y){
+          z++;
+          while (SDL_PollEvent(&event)){
+            switch (event.type) {
+                case SDL_QUIT:{
+                    
+                    break;
+                }  
+               case SDL_MOUSEBUTTONDOWN:{
+                  int mouse_x = event.button.x/Width;
+                  int mouse_y = event.button.y/High;
+                  if(event.button.button == SDL_BUTTON_LEFT){
+                      change(mouse_x,mouse_y,cells);
+                     }
+                   
+                }
+               
+            }
+        }
+
+      
+        if(last!=NULL){
+           d=judge(cells,last);
+        } 
+           if(d==0){
+            printf("It is terminate\n");
+                m=0;
+                while(m==0){
+                printf("Do you want to show the next steps?(input 0 for quit and 1 for continue)\n");
+                z=optionChoice();
+                if(z==-1){
+             printf("\nplease input 0 or 1\n\n");
+                }
+                else if(z!=0&&z!=1){
+                    printf("please input 0 or 1\n\n");
+                }
+                else if(z==1){
+                    m=1;
+                }
+                else if(z==0){
+                 printf("The world is terminated\n");
+                      freecell(last);
+                    close(sdlWindow,sdlTexture,sdlRenderer);
+                  return terminate;
+                }
+        }
+           }
+
+           printf("step %d:\n",z);
+           free(last);
+           last=copy(cells);
+         show(sdlRenderer,sdlWindow,sdlTexture, cells);
+             updateWithoutInput(cells);
+           if(z!=y){
+           printf("\n");        
+           }
+           else{
+               printf("The step is up\n");
+               my=copy(cells);
+               updateWithoutInput(my);
+               d=judge(my,last);
+               
+                
+               if(d==0){
+                   freecell(my);
+                   freecell(last);
+                      close(sdlWindow,sdlTexture,sdlRenderer);
+                   return terminate;
+               }
+               else{
+                  freecell(my);
+                   freecell(last);
+                      close(sdlWindow,sdlTexture,sdlRenderer);
+                   return still;
+               }
+           }
+          
+         
+    }
+        
+    
    
-    int i,j;
-    for(i=0;i<High;i++)
-        for(j=0;j<Width;j++)
-            cells[i][j]=1;                         
+
+    return 100;
 }
 
 
 int  game(FILE* file,int ** cell){
-   int **last=NULL;
-    int **my;
-   int q;
-    int d;
-    
+    int result;
      int x;
      int a=0;
      int b=0;
@@ -595,37 +822,16 @@ int  game(FILE* file,int ** cell){
    }
      }
      if(x==2){
-       q=0;
-    while(1){
-       if(last!=NULL){
-        d=judge(cell,last);
-        if(d==0){
-            printf("It is terminate\n");
-            return terminate;
-        }
-       }    
-       if(q==0){
-           printf("The origin world:\n");
-            last=copy(cell);
-            show(cell);
-            updateWithoutInput(cell);
-            printf("\n");
-            q+=1;
-       }
-       else{
-        printf("step %d:\n",q);
-        free(last);
-        last=copy(cell);
-        show(cell);
-        updateWithoutInput(cell);
-         printf("\n");
-         q+=1;
-       }
-    }
+      result= run2(cell);
+      if(result==terminate){
+          return terminate;
+      }
+      else{
+          return still;
+      }
      }
+     
      else{
-         int m=0;
-        int z;
         while(b==0){
          printf("Please input the number of evolution:");
           x=optionChoice();
@@ -641,66 +847,16 @@ int  game(FILE* file,int ** cell){
               b=1;
           }
    }
-        printf("The origin world:\n");
-        show(cell);
-        last=copy(cell);
-        printf("\n"); 
+   result=run1(cell,x);
+    if(result==terminate){
+       
+          return terminate;
+      }
+      else{
+       
+          return still;
+      }
         
-         for(int i=0;i<x;i++){
-              
-          updateWithoutInput(cell);
-           d=judge(cell,last);
-           if(d==0){
-            printf("It is terminate\n");
-                m=0;
-                while(m==0){
-                printf("Do you want to show the next steps?(input 0 for quit and 1 for continue)\n");
-                z=optionChoice();
-                if(z==-1){
-             printf("\nplease input 0 or 1\n\n");
-                }
-                else if(z!=0&&z!=1){
-                    printf("please input 0 or 1\n\n");
-                }
-                else if(z==1){
-                    m=1;
-                }
-                else if(z==0){
-                 printf("The world is terminated\n");
-                      freecell(last);
-                  return terminate;
-                }
-                }
-            
-        }
-          
-           printf("step %d:\n",i+1);
-           show(cell);
-           freecell(last);
-           last=copy(cell);
-           if(i!=x-1){
-           printf("\n");        
-           }
-           else{
-               printf("The step is up\n");
-               my=copy(cell);
-               updateWithoutInput(my);
-               d=judge(my,last);
-               
-                
-               if(d==0){
-                   freecell(my);
-                   freecell(last);
-                   return terminate;
-               }
-               else{
-                  freecell(my);
-                   freecell(last);
-                   return still;
-               }
-           }
-          
-         }
      }
    return 1;
 }
@@ -716,7 +872,6 @@ void  game2(char * filename){
     int a;
      while(1){
          a=0;
-        init();
         result=game(file,cell);
         if(result==terminate){
             while(a==0){
@@ -737,6 +892,7 @@ void  game2(char * filename){
              }
         }
         if(x==0){
+             
              printf("\nGame closed,Bye Bye\n");
              store(close,cell);
              freecell(cell);
@@ -744,12 +900,14 @@ void  game2(char * filename){
              return;
         }
         if(x==1){
+             
             free(cell);
            cell = create();
            continue;
         }
         }
         if(result==still){
+            
             while(a==0){
              printf("This world is not terminated\n");           
              printf("Please choose an option\n1)Quit the game\n2)Still use it for another game\n3)Create a new world\n");
@@ -760,6 +918,7 @@ void  game2(char * filename){
                   continue;
              }
              else if(x==1){
+                 
                 printf("\nGame closed,Bye Bye\n\n");
                 
                  store(close,cell);
@@ -774,6 +933,7 @@ void  game2(char * filename){
                  a=1;
              }
              else if(x==3){
+                 
                 freecell(cell);
                 cell=create();   
                 a=1;
